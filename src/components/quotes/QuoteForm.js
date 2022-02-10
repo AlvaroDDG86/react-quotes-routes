99bc82a1de7a1ff537e376cfbbd7e30b05c42388
AlvaroDDG86 <alvarodediosgarcia1986@gmail.com>
2022-02-10 22:01:16 +0100

    quotes components

---
+++ src/components/quotes/QuoteForm.js
@@ -0,0 +1,58 @@
+import { Fragment, useRef, useState } from "react";
+import { Prompt } from "react-router-dom";
+
+import Card from "../UI/Card";
+import LoadingSpinner from "../UI/LoadingSpinner";
+import classes from "./QuoteForm.module.css";
+
+const QuoteForm = (props) => {
+  const authorInputRef = useRef();
+  const textInputRef = useRef();
+  const [isFormTouched, setIsFormTouched] = useState(false);
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+
+    const enteredAuthor = authorInputRef.current.value;
+    const enteredText = textInputRef.current.value;
+
+    // optional: Could validate here
+    props.onAddQuote({ author: enteredAuthor, text: enteredText });
+  }
+
+  const foucsHandler = () => {
+    setIsFormTouched(true);
+  };
+
+  return (
+    <Fragment>
+      <Prompt when={isFormTouched} message={() => 'You want to leave?'}/>
+      <Card>
+        <form
+          className={classes.form}
+          onSubmit={submitFormHandler}
+          onFocus={foucsHandler}
+        >
+          {props.isLoading && (
+            <div className={classes.loading}>
+              <LoadingSpinner />
+            </div>
+          )}
+          <div className={classes.control}>
+            <label htmlFor="author">Author</label>
+            <input type="text" id="author" ref={authorInputRef} />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="text">Text</label>
+            <textarea id="text" rows="5" ref={textInputRef}></textarea>
+          </div>
+          <div className={classes.actions}>
+            <button onClick={() => setIsFormTouched(false)} className="btn">Add Quote</button>
+          </div>
+        </form>
+      </Card>
+    </Fragment>
+  );
+};
+
+export default QuoteForm;
