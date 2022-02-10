edcabccdce021a1bb8c137fbc85bd5979061bd7b
AlvaroDDG86 <alvarodediosgarcia1986@gmail.com>
2022-02-10 22:00:39 +0100

    layouts

---
+++ src/components/layout/MainNavigation.js
@@ -0,0 +1,23 @@
+import { NavLink } from 'react-router-dom'
+import classes from './MainNavigation.module.css'
+const MainNavigation = () => {
+    return (
+        <header className={classes.header}>
+            <div className={classes.logo}>
+                Quotes
+            </div>
+            <nav className={classes.nav}>
+                <ul>
+                    <li>
+                        <NavLink activeClassName={classes.active} to="/quotes">All Quotes</NavLink>
+                    </li>
+                    <li>
+                        <NavLink activeClassName={classes.active} to="/quotes/add">Add a Quote</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default MainNavigation
